fe2e9f685ad0fff042bcf811049aed18a79db5c6
CoderHook <Abel1987@hotmail.es>
2016-07-22 10:38:48 +0000

    Fixed the software Regex expresion

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.get('/', function(req, res){
     
     var ipadress = req.headers['x-forwarded-for'];
     var lang = req.headers['accept-language'].match(/^[a-z]{2}\-[A-Z]{2}/).toString();
-    var software = req.headers['user-agent'].match(/\w+\s\w+\s\d(\.\d)?\;\s\w+\d+\;\s\w+\d+/gim).toString();
+    var software = req.headers['user-agent'].match(/\(([^\)]+)\)/);
     
-    res.send({"ipaddress":ipadress, "language": lang, "software": software});
-    console.log(req.headers);
+    console.log(req.headers['user-agent']);
+    res.send({"ipaddress":ipadress, "language": lang, "software": software[1]});
+    //console.log(req.headers);
 });
 
 app.listen(process.env.PORT || 8080, function(){
